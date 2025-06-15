184c638dd05586749bb80fa072ab34d7499292b3
ljx <1617929300@qq.com>
2025-06-15 22:12:44 +0800

    完成 team 相关的最基本的 增删改查

---
+++ src/main/java/com/yupi/demo2/common/PageRequest.java
@@ -0,0 +1,37 @@
+package com.yupi.demo2.common;
+
+import java.io.Serializable;
+
+/**
+ * 通用分页请求参数
+ */
+public class PageRequest implements Serializable {
+
+    private static final long serialVersionUID = 2L;
+
+    /**
+     * 页面大小
+     */
+    protected int pageSize = 10;
+
+    /**
+     * 当前是第几页
+     */
+    protected int pageNum = 1;
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public int getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(int pageNum) {
+        this.pageNum = pageNum;
+    }
+}
---
+++ src/main/java/com/yupi/demo2/model/dto/TeamQuery.java
@@ -0,0 +1,96 @@
+package com.yupi.demo2.model.dto;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.yupi.demo2.common.PageRequest;
+import lombok.EqualsAndHashCode;
+
+import java.util.Date;
+
+/**
+ * 队伍查询封装类
+ *
+ */
+@EqualsAndHashCode(callSuper = true)
+public class TeamQuery extends PageRequest {
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 队伍名称
+     */
+    private String name;
+
+    /**
+     * 描述
+     */
+    private String description;
+
+    /**
+     * 最大人数
+     */
+    private Integer maxNum;
+
+    /**
+     * 用户id
+     */
+    private Long userId;
+
+    /**
+     * 状态 0-公开, 1-私有, 2-加密
+     */
+    private Integer status;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getMaxNum() {
+        return maxNum;
+    }
+
+    public void setMaxNum(Integer maxNum) {
+        this.maxNum = maxNum;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
